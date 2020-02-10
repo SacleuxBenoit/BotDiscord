c12da37396e9d4f4cb59e724acba5e53a1c6b308
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-10 20:23:01 +0100

    add message.channel.send in index.js

--- index.js
+++ index.js
@@ -2,4 +2,9 @@ const Discord = require('discord.js');
 const Token = require('./tokenBot');
 const bot = new Discord.Client()
 
+bot.on('message', function(message){
+    if (message.content === '!ping'){
+        message.channel.send('pong')
+    }
+})
 bot.login("Njc2NDg5ODM5MDQxODM5MTcx.XkGdPg.SsyOeVTSMOVSxpW55E5LskW-QBk")
