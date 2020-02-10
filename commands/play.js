d8fcefd2f82895ffe2fc2c4798a8c35814eb1820
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-10 21:37:50 +0100

    add content in play.js

---
+++ commands/play.js
@@ -0,0 +1,9 @@
+const Command = require('./command')
+module.exports = class Play extends Command{
+
+    static match(message){
+        return message.content.startsWith('!play')
+    }
+    static action (message){
+    }
+}
