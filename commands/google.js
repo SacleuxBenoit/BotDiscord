8ab9a4c820356bb4274c9571a47e3bcf356a4c9a
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-10 21:23:29 +0100

    add the function message.delete in google.js

--- commands/google.js
+++ commands/google.js
@@ -7,6 +7,7 @@ module.exports = class Google extends Command{
     static action (message){
        let args = message.content.split(' ')
         args.shift()
+        message.delete()
         message.reply('https://www.google.fr/#q=' + args.join('%20'))
     }
     
