56190a7c2b46b3746cfc76b1a8ed35d43e660527
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-10 21:08:39 +0100

    add the import of command in google.js

--- commands/google.js
+++ commands/google.js
@@ -1,4 +1,5 @@
-module.exports = class Google{
+const Command = require('./command')
+module.exports = class Google extends Command{
 
     static match(message){
         return message.content.startsWith('!google')
