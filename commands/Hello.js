b85fe7fd4b6dd8f3c27b4ce97f349078f39598ce
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-12 14:02:44 +0100

    change Hello to hello in hello.js

--- commands/Hello.js
+++ commands/Hello.js
@@ -2,7 +2,7 @@ const Command = require('./command')
 module.exports = class Hello extends Command{
 
     static match(message){
-        return message.content.startsWith('!Hello')
+        return message.content.startsWith('!hello')
     }
     static action (message){
         message.reply('Hola')
